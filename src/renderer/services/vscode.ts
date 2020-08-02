e75b2bf857910381c24ede75b405c1e8300c8263
AngryPowman <powmanx2@gmail.com>
2020-08-03 04:30:16 +0800

    fix: find vscode bin on windws

--- src/renderer/services/vscode.ts
+++ src/renderer/services/vscode.ts
@@ -1,40 +1,48 @@
 import fs from "fs-extra";
 import { execFile } from "child_process";
 import { Env } from "../../common/env";
-import regedit from "regedit";
+import commandExists from "command-exists";
 
 export class VSCode {
   static async run(projectDir: string) {
-    const exception = "无法找到 VSCode 的可执行文件，请确保已正确安装 Visual Studio Code .";
+    const exception =
+      "无法找到 VSCode 的可执行文件，请确保已正确安装 Visual Studio Code .";
     let codeBin = "";
     if (Env.getOSName() === "MacOS") {
       codeBin =
         "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code";
     } else if (Env.getOSName() === "Windows") {
       const key = `HKCU\\Software\\Classes\\*\\shell\\VSCode`;
-      codeBin = await new Promise<string>((resolve, reject) => {
-        regedit.list(
-          key,
-          (
-            err: any,
-            result: { [x: string]: { values: { [x: string]: { value: any } } } }
-          ) => {
-            if (err) {
-              reject();
-            }
-
-            const reg = result[key].values["Icon"];
-            if (reg) {
-              const value = reg.value;
-              resolve(value);
-            }
-
-            reject()
-          }
-        );
-      }).catch(() => {
+      if (commandExists.sync("code")) {
+        codeBin = "code";
+      } else {
         throw exception;
-      })
+      }
+
+      // commandExists.
+      // codeBin = await new Promise<string>((resolve, reject) => {
+      //   regedit.list(
+      //     key,
+      //     (
+      //       err: any,
+      //       result: { [x: string]: { values: { [x: string]: { value: any } } } }
+      //     ) => {
+      //       if (err) {
+      //         reject();
+      //       }
+
+      //       const reg = result[key].values["Icon"];
+      //       if (reg) {
+      //         const value = reg.value;
+      //         resolve(value);
+      //       }
+
+      //       reject()
+      //     }
+      //   );
+      // }).catch(() => {
+      //   throw exception;
+      // })
     }
 
     const parameters = [projectDir];
