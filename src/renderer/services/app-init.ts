11ed53d73d8ea93e8b2ff14b2722222ad7b59417
AngryPowman <powmanx2@gmail.com>
2020-08-20 15:56:52 +0800

    fix: remove autoupdator initialize

--- src/renderer/services/app-init.ts
+++ src/renderer/services/app-init.ts
@@ -2,7 +2,6 @@ import os from "os";
 import { FocusStyleManager } from "@blueprintjs/core";
 
 import "../../common/local-app-config";
-import { AutoUpdater } from "./autoupdater";
 import { logger } from "../../common/lib/logger";
 import { remote } from "electron";
 import { Env } from "../../common/env";
@@ -15,8 +14,5 @@ export class AppInit {
 
     // Blueprint 焦点设置
     FocusStyleManager.onlyShowFocusOnTabs();
-
-    // 初始化自动更新
-    AutoUpdater.init();
   }
 }
