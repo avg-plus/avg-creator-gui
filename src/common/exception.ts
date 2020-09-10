32169e1f4457b8b21f9fb74843f4f03579347b2c
AngryPowman <powmanx2@gmail.com>
2020-09-10 18:14:34 +0800

    feat: add assert

--- src/common/exception.ts
+++ src/common/exception.ts
@@ -1,3 +1,12 @@
-export class Exception {
-  constructor(desc: string) {}
+export function assert(
+  expected: boolean | any,
+  message: string,
+  cb?: () => void
+) {
+  if (!expected) {
+    cb && cb();
+    throw new Error(message);
+  }
 }
+
+export class Exception {}
